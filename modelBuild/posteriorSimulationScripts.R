4a698b68ce8b2a071493c60438a9689615866fa4
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-02-10 14:47:03 -0500

    ppc simulations for seeds in January

--- modelBuild/posteriorSimulationScripts.R
+++ modelBuild/posteriorSimulationScripts.R
@@ -1,19 +1,45 @@
 
+rm(list=ls(all=TRUE)) # clear R environment
+
 load(file="/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/output/seedbagfit.rds")
 load(file="/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/output/seedbagdata.rds")
 
 getParameters <- function(codaObject,x) { codaObject[,stringr::str_detect(colnames(codaObject),x)] }
-  
-aS1 <- getParameters(zc[[1]],"alphaS1")
-ps<-apply(aS1,2,boot::inv.logit)
 
+## Seeds in seed bags in January
+parameter = "alphaS1"
+
+  merged.data.frame = Reduce(function(...) merge(..., all=T), zc)
+  
+  aS1 <- getParameters(merged.data.frame,parameter)
+  ps<-apply(aS1,2,boot::inv.logit)
+
+
+tmp <- matrix(nrow=3000,ncol=535)
 for(i in 1:length(data$n)){
-  rbinom(n=1,size=data$n[1],p=ps[,1])
-rbinom(data$n
+  tmp[,i]<-rbinom(n=length(ps[,data$site[i]]),size=data$n[i],p=ps[,data$site[i]])
 }
 
-# yv.sim[i] ~ dbinom(p[i], nv[i]) 
+samps<-sample(3000,1000)
 
+bayesplot::ppc_dens_overlay(data$yt, tmp[samps,]) +
+  theme_bw() + xlim(c(0,100)) + labs(title="Posterior predictive checks for seeds counted in seed bags in January", 
+                                     caption="Dark line is the density of observed data (y) and the lighterlines show the densities of Y_rep from 1000 draws of the posterior")
+
+
+bayesplot::ppc_stat_grouped(data$yt, tmp[samps,], group=interaction(data$site)) +
+  theme_bw() + labs(title="Posterior predictive checks for the mean of seeds counted in seed bags in January", 
+                    caption="the bar is the observed value of test statistic T(y) and the histograms show T(Y_rep) from 1000 draws of the posterior")  
+
+bayesplot::ppc_stat_grouped(data$yt, tmp[samps,],group=data$site, stat="var") +
+  theme_bw() + labs(title="Posterior predictive checks for the variance of seeds counted in seed bags in January", 
+                    caption="the bar is the observed value of test statistic T(y) and the histograms show T(Y_rep) from 1000 draws of the posterior")
+
+# Seed survival (g1)
+
+# Seed survival (s2)
+
+# Seed survival (s3)
 
 ps[i] = ilogit(alphaS1[site[i]])
 
