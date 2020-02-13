aee3e80d5d732fc27fabe27be3db757b287b15c1
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-02-13 13:30:05 -0500

    add partial pooling model

---
+++ modelBuild/jagsScripts/seedBagsPartialPoolingJAGS.R
@@ -0,0 +1,56 @@
+
+model { 
+    
+    ##############
+    ## priors
+    ##############
+    
+    for(j in 1:nbags){
+        # lab experiments
+        pg[j] ~ dbeta( 1 , 1 )
+        pv[j] ~ dbeta( 1 , 1 )
+    }
+    
+    for(j in 1:nbags){
+        # site intercepts
+        pi[j] ~ dbeta( 1 , 1 )
+        ps[j] ~ dbeta( 1 , 1 )
+    }
+    
+    ##############
+    ## likelihoods
+    ##############
+    
+    # germination and viability experiments
+    for(i in 1:N){
+        # g germination
+        yg[i] ~ dbinom( pg[bag[i]] , ng[i] )
+        
+        # v viability
+        yv[i] ~ dbinom( pv[bag[i]] , nv[i] )
+        
+        ygSim[i] ~ dbinom( pg[bag[i]] , ng[i] )
+        yvSim[i] ~ dbinom( pv[bag[i]] , nv[i] )
+        
+    }
+
+    # calculate viability    
+    for(j in 1:nbags){
+        viability[j] = pg[j] + pv[j]*(1-pg[j])
+    }
+
+    # seed burial experiments
+    for(i in 1:N_burial){
+        
+        # s1 seed survival
+        y_total[i] ~ dbin(pi[bag_burial[i]], n_buried[i])
+        
+        # g1 seed germination
+        y_seedlings[i] ~ dbin(ps[bag_burial[i]]*(viability[bag_burial[i]]^(1/3)), y_total[i])
+        
+        ySeedlingsSim[i] ~ dbinom(ps[bag_burial[i]]*(viability[bag_burial[i]]^(1/3)), y_total[i])
+        yTotalSim[i] ~ dbin(pi[bag_burial[i]], n_buried[i])
+    }
+    
+}
+
