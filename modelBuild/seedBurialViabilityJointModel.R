fa5fe176011e243b302f7cf99f1683c716444a6c
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-02-13 10:12:02 -0500

    add code to prep data for JAGS

--- modelBuild/seedBurialViabilityJointModel.R
+++ modelBuild/seedBurialViabilityJointModel.R
@@ -37,7 +37,7 @@ df$totalJan <- ifelse(df$totalJan>100,100,df$totalJan)
 
 # `MC III 5 13 1 is a problem (91 intact, 17 seedlings)`
 
-df <- df %>% dplyr::rename(bagNo=bag)
+# df <- df %>% dplyr::rename(bagNo=bag)
 
 ## FOR NOW REMOVE MISSING DATA AND PROBLEMS
 # the main issue will be bags that were not recovered in January
@@ -62,7 +62,7 @@ df <- viabilityRawData
 df <- df %>% 
   dplyr::select(-c(germPerc,germNot,viabPerc,viabPerc2,condTest))
 
-df <- df %>% dplyr::rename(bagNo=bag)
+#df <- df %>% dplyr::rename(bagNo=bag)
 df$bag <-as.integer(as.numeric(df$bagNo))
 
 viabilityExperiment <- df
@@ -95,10 +95,10 @@ viabilityExperiment<-viabilityExperiment %>%
 
 # -------------------------------------------------------------------
 # -------------------------------------------------------------------
-# Multiple-site model (3 year)
-# Log-odds parameterization for viability, trials summed across bags
+# Prepare data for analysis with JAGS
 # -------------------------------------------------------------------
 # -------------------------------------------------------------------
+
 
 l1<-seedBagExperiment %>%
   dplyr::select(site,bagNo,round,age) %>%
@@ -155,77 +155,11 @@ data = list(
   nbags = length(unique(viabilityExperiment$siteBag))
 )
 
-# pass data to list for JAGS
-# data = list()
-#    yg = as.double(dat$seedlingJan),
-#   yt = as.double(dat$totalJan),
-#   yo = as.double(dat$intactOct),
-#   yv = as.double(dat$y_new),
-#   n = as.double(dat$seedStart),
-#   nv = as.double(dat$n_new),
-#   N = nrow(dat),
-#   site = as.double(dat$site.index),
-#   nsites = length(unique(dat$site.index)),
-#   year = as.double(dat$year.index),
-#   nyears = length(unique(dat$year.index)),
-#   
-#   yt2 = as.double(datTwo$totalJan),
-#   n2 = as.double(datTwo$seedStart),
-#   N2 = nrow(datTwo),
-#   site2 = as.double(datTwo$site.index),
-#   nsites2 = length(unique(datTwo$site.index)),
-#   year2 = as.double(datTwo$year.index),
-#   nyears2 = length(unique(datTwo$year.index))
-# )
-
-# right now each bag has its own prior;
-# want to give nsite priors 
-# inits = list(
-#   list( sigma.b = matrix(rep(50,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears), 
-#         mu.b = matrix(rep(0,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears), 
-#         #alpha = rep(-10, data$N),
-#         # sigmaS1 = rep(.1, data$nsites), 
-#         # sigmaG1 = rep(.1, data$nsites), 
-#         # sigmaS2 = rep(.1, data$nsites),
-#         alphaS1 = rep(-10, data$nsites), 
-#         alphaG1 = rep(-10, data$nsites), 
-#         alphaS2 = rep(-10, data$nsites),
-#         alphaS3 = rep(0, data$nsites)
-#         # betaS1 = matrix(-10, nrow=data$nsites,ncol=data$nyears), 
-#         # betaG1 = matrix(-10, nrow=data$nsites,ncol=data$nyears), 
-#         # betaS2 = matrix(-10, nrow=data$nsites,ncol=data$nyears)
-#   ),
-#   list( sigma.b = matrix(rep(10,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears), 
-#         mu.b = matrix(rep(0,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears),
-#         #alpha = rep(0, data$N),
-#         # sigmaS1 = rep(.5, data$nsites), 
-#         # sigmaG1 = rep(.5, data$nsites), 
-#         # sigmaS2 = rep(.5, data$nsites),
-#         alphaS1 = rep(0, data$nsites), 
-#         alphaG1 = rep(0, data$nsites), 
-#         alphaS2 = rep(0, data$nsites),
-#         alphaS3 = rep(0, data$nsites)
-#         # betaS1 = matrix(0, nrow=data$nsites,ncol=data$nyears), 
-#         # betaG1 = matrix(0, nrow=data$nsites,ncol=data$nyears), 
-#         # betaS2 = matrix(0, nrow=data$nsites,ncol=data$nyears)
-#   ),
-#   list( sigma.b = matrix(rep(20,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears), 
-#         mu.b = matrix(rep(0,data$nsites*data$nyears),nrow=data$nsites,ncol=data$nyears), 
-#         #alpha = rep(10, data$N),
-#         #sigmaS1 = rep(.9, data$nsites), 
-#         #sigmaG1 = rep(.9, data$nsites), 
-#         #sigmaS2 = rep(.9, data$nsites),
-#         alphaS1 = rep(10, data$nsites), 
-#         alphaG1 = rep(10, data$nsites), 
-#         alphaS2 = rep(10, data$nsites),
-#         alphaS3 = rep(0, data$nsites)
-#         # betaS1 = matrix(10, nrow=data$nsites,ncol=data$nyears), 
-#         # betaG1 = matrix(10, nrow=data$nsites,ncol=data$nyears), 
-#         # betaS2 = matrix(10, nrow=data$nsites,ncol=data$nyears)
-#   )
-# )
-
-
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+# Set JAGS parameters and random seed
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
 # scalars that specify the 
 # number of iterations in the chain for adaptation
 # number of iterations for burn-in
@@ -233,28 +167,35 @@ data = list(
 n.adapt = 1000
 n.update = 10000
 n.iter = 10000
+n.thin = 10
+
+set.seed(10)
+dir = c("/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/jagsScripts/")
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+# Complete pooling
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+# set inits for JAGS
+inits = list(list(pv = .1,pg = .1), 
+             list(pv = .5,pg = .5), 
+             list(pv = .9,pg = .9))
 
 # Call to JAGS
 
-dir = c("/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/jagsScripts/")
-
 # tuning (n.adapt)
-jm = jags.model(paste0(dir,"seed_modelJAGS.R"), data = data, inits = inits,
+jm = jags.model(paste0(dir,"viabilityJointCompletePoolingJAGS.R"), data = data, inits = inits,
                 n.chains = length(inits), n.adapt = n.adapt)
 
 # burn-in (n.update)
 update(jm, n.iter = n.update)
 
-intercepts = c("alphaS1","alphaG1","alphaS2","alphaS3")
-#slopes = c("betaS1","betaG1","betaS2")
-#variances = c("sigmaS1","sigmaG1","sigmaS2")
-viab = c("mu.b","sigma.b")
-#sims = c("yv.sim","yt.sim","yg.sim","yo.sim","yt2.sim")
-
-
+viab = c("pv","pg","viability")
 
 # chain (n.iter)
-zc = coda.samples(jm, variable.names = c(intercepts,viab), n.iter = n.iter, thin=10)
+zc_pool = coda.samples(jm, variable.names = c(viab), n.iter = n.iter, thin = n.thin)
 
-save(zc,file="/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/output/seedbagfit.rds")
-save(data,file="/Users/Gregor/Dropbox/clarkiaSeedBanks/modelBuild/output/seedbagdata.rds")
+MCMCsummary(zc_pool, params = c("pv","pg","viability"))
+save(zc_pool,file="/Users/Gregor/Dropbox/dataLibrary/clarkiaSeedBanks/viabilityJointCompletePoolFit.rds")
