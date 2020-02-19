e409828c6bf983333fe1502e6cfa95fac7f1a339
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-02-19 16:59:10 -0500

    add sum to zero constraint

--- modelBuild/jagsScripts/testJags.R
+++ modelBuild/jagsScripts/testJags.R
@@ -3,20 +3,20 @@ model {
 ##hyperprior for intercept alpha
   
   mu.alpha ~ dnorm(0, 0.0001)
-  
-    sigma.year~dunif(0,100)
-    tau.year<-1/(sigma.year*sigma.year)
 
   for(j in 1:nsiteyears){
-    beta.i[j] ~ dnorm(0, tau.year)
+    beta.i[j] ~ dnorm(0, 0.0001)
   }
+  
+  # sum to zero constraint
+  alpha.i[1] <- 0 - sum( beta.i[2:N.plots])
+  alpha.i[2:N.plots] <- beta.i[2:N.plots]
 
 ## Likelihood
 for(i in 1:N_burial){
-    pi[i] <- ilogit(mu.alpha + beta.i[siteyear[i]])
+    pi[i] <- ilogit(mu.alpha + alpha.i[siteyear[i]])
   y_total[i] ~ dbin(pi[i], n_buried[i])
 
 }
 }
-
 
