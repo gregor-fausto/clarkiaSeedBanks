d8406b6ea07791562ca1f120d262e67dab7c8367
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-02-20 12:32:14 -0500

    update test files

---
+++ modelBuild/jagsScripts/seedBagsPartialPoolingViabilityPartialPoolingLogitSiteYearSTZJAGS.R
@@ -0,0 +1,100 @@
+
+model { 
+    ##############
+    ## hyperpriors
+    ##############
+    
+    # for(i in 1:nsites){
+    #     mu.i[i] ~ dnorm( 0, 0.0001 )
+    #     # sigma.i[i] ~ dunif(0,100)
+    #     # tau.i[i] <- 1/(sigma.i[i] * sigma.i[i])
+    #     
+    #     # mu.s[i] ~ dnorm( 0, 0.0001 )
+    #     # sigma.s[i] ~ dunif(0,100)
+    #     # tau.s[i] <- 1/(sigma.s[i] * sigma.s[i])
+    # }
+    
+    # for(i in 1:nsiteyears) {   
+    #     mu.b.i[i] ~ dnorm( 0, 0.0001 )
+    #     sigma.b.i[i] ~ dunif(0,100)
+    #     tau.b.i[i] <- 1/(sigma.b.i[i] * sigma.b.i[i])
+    #     
+    #     # mu.b.s[i] ~ dnorm( 0, 0.0001 )
+    #     # sigma.b.s[i] ~ dunif(0,100)
+    #     # tau.b.s[i] <- 1/(sigma.b.s[i] * sigma.b.s[i])
+    # }
+    
+    ##############
+    ## priors
+    ##############
+    
+    # for(j in 1:nbags){
+    #     # lab experiments
+    #     pg[j] ~ dbeta( 1 , 1 )
+    #     pv[j] ~ dbeta( 1 , 1 )
+    # }
+    # 
+    # for(j in 1:nsites){
+    #     #site intercepts
+    #     alpha.i[j] ~ dnorm(mu.i[site[j]], tau.i[site[j]])
+    #    # alpha.s[j] ~ dnorm(mu.s[site[j]], tau.s[site[j]])
+    # }
+    
+    for(i in 1:nsites){
+        alpha.i[i] ~ dnorm(0, 0.0001)
+        for(k in 1:nyears){
+            beta.0.i[i,k] ~ dnorm(0, 0.0001)
+        }
+    }
+    
+    # sum to zero constraint
+    for(i in 1:nsites){
+        beta.i[i,1] <- 0 - sum( beta.0.i[i,2:nyears] )
+        beta.i[i,2:nyears] <- beta.0.i[i,2:nyears]
+    }
+    
+    # for(j in 1:nsiteyears){
+    #     beta.i[j] ~ dnorm(mu.b.i[siteyear[j]], tau.b.i[siteyear[j]])
+    #     #beta.s[j] ~ dnorm(mu.b.s[siteyear[j]], tau.b.s[siteyear[j]])
+    # }
+    
+    ##############
+    ## likelihoods
+    ##############
+    
+    # germination and viability experiments
+    # for(i in 1:N){
+    #     # g germination
+    #     yg[i] ~ dbinom( pg[bag[i]] , ng[i] )
+    #     
+    #     # v viability
+    #     yv[i] ~ dbinom( pv[bag[i]] , nv[i] )
+    #     
+    #     ygSim[i] ~ dbinom( pg[bag[i]] , ng[i] )
+    #     yvSim[i] ~ dbinom( pv[bag[i]] , nv[i] )
+    #     
+    # }
+    
+    # calculate viability    
+    # for(j in 1:nbags){
+    #     viability[j] = pg[j] + pv[j]*(1-pg[j])
+    # }
+    # 
+    # seed burial experiments
+    for(i in 1:N_burial){
+
+        # s1 seed survival
+        pi[i] <- ilogit(alpha.i[site[i]] + beta.i[site[i],year[i]])
+        y_total[i] ~ dbin(pi[i], n_buried[i])
+        
+        # g1 seed germination
+        # ps[i] <- ilogit(alpha.s[site[i]]+beta.s[siteyear[i]])
+        # 
+        # y_seedlings[i] ~ dbin(ps[i]*pi[i]*(viability[bag_burial[i]]^(1/3)), n_buried[i])
+        # 
+        # ySeedlingsSim[i] ~ dbinom(ps[i]*pi[i]*(viability[bag_burial[i]]^(1/3)), n_buried[i])
+        yTotalSim[i] ~ dbin(pi[i], n_buried[i])
+    }
+    
+    }
+    
--- modelBuild/jagsScripts/testMultiYear2JAGS.R
+++ modelBuild/jagsScripts/testMultiYear2JAGS.R
@@ -8,25 +8,28 @@ model {
     mu.alpha[i] ~ dnorm(0, tau.site[i])
     for(k in 1:nyears){
       beta.i[i,k] ~ dnorm(0, 0.0001)
-      }
+    }
   }
   
   # sum to zero constraint
   for(i in 1:nsites){
-    alpha.i[i,1] <- 0 - sum( beta.i[i,2:nyears] )
-    alpha.i[i,2:nyears] <- beta.i[i,2:nyears]
+    mu_adj[i] = mu.alpha[i] + mean(beta.i[i,])
+    for(k in 1:nyears){
+      beta.i_adj[i,k] = beta.i[i,k] - mean(beta.i[i,])
+      }
   }
   
   ## Likelihood
   for(i in 1:N_burial){
-    pi[i] <- ilogit(mu.alpha[site[i]] + alpha.i[site[i],year[i]])
+    pi[i] <- ilogit(mu.alpha[site[i]] + beta.i[site[i],year[i]])
     y_total[i] ~ dbin(pi[i], n_buried[i])
     yTotalSim[i] ~ dbin(pi[i], n_buried[i])
   }
   
   for(i in 1:nsites){
     for(j in 1:nyears){
-      pred[i,j] <- mu.alpha[i] + alpha.i[i,j]
+      pred[i,j] <- mu.alpha[i] + beta.i[i,j]
+      pred_adj[i,j] <- mu_adj[i] + beta.i_adj[i,j]
     }
   }
 }
