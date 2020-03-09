5b80555f275941a4e0cb66bcc3b7294ed4cd3343
Gregor Siegmund <fausto.siegmund@gmail.com>
2020-03-09 15:57:11 -0400

    add s3 calculations

--- reshapeDataScripts/germinationProbabilityCalculations.R
+++ reshapeDataScripts/germinationProbabilityCalculations.R
@@ -3,11 +3,14 @@ total = 100
 # number intact but not germinated
 intact = 60
 # number germinated
-germinant = 10
+germinant = 20
 # number intact in october
-october.intact = 20
+october.intact = 30
 # number intact in january 2
-january.intact = 5
+january.2.intact = 10
+# number germinated in january 2
+january.2.germ = 10
+
 
 # method 1
 prob.germination = function( n = intact, y = germinant, p.v = v ){
@@ -25,27 +28,14 @@ prob.s2 = function( n = intact, y =germinant, y.o = october.intact, p.v = v ){
   return(p)
 }
 
-prob.s3 = function( n = intact, y =germinant, y.o = october.intact, y.j = january.intact, p.v = p , t = total){
-  s1 = prob.s1(n = n, y =y, y.o = y.o, p.v = p.v, t = t )
-  g1c = (1-prob.germination(n = n, y = y, p.v = p.v))
-  s2 = prob.s2(n=n, y=y, y.o=y.o, p.v=p.v)
-  s3 = january.intact/october.intact
-  p = s1*g1c*s2*s3
+prob.s3 = function( n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total){
+  
+  p.vj = ifelse(p.v2 < p.v, p.v^(2/3)*p.v2^(1/3), p.v2^(1/3))
+  
+  p = ((p.vj^(1/3))*y.j+y.g2)/(y.o*p.v)
   return(p)
 }
-# 
-# I=find(strcmp(Pop,PopNow) & Round==r & Age==a & ~isnan(JanIntact)); % Note: JanTot,Germ,Intact either all estimated or all missing
-# NBs3(p,r)=length(I);
-# % est. total no. of ungerm. seeds still alive after 1 yr. - in Oct.
-# SSs3(p,r)=100*s1(p,r)*(1-g1(p,r))*s2(p,r)*NBs3(p,r); 
-# if PV2(p,r)<PV1(p,r)
-# PVjan=(PV1(p,r)^.66667)*(PV2(p,r)^.33333);
-# else
-#   PVjan=PV2(p,r)^.33333;
-# end
-# SSg2(p,r)=sum( JanGerm(I)+ PVjan*JanIntact(I) ); % SS for estimating g2 is no. of VIABLE seeds in Jan.
-# s3(p,r)=min(SSg2(p,r)/SSs3(p,r),1);
-# g2(p,r)=sum( JanGerm(I) )/SSg2(p,r);     
+ 
 
 
 # method 2
@@ -70,59 +60,99 @@ prob.s2.2 = function( n = intact, y =germinant, y.o = october.intact, p.v = v ){
 }
 
 
-par(mfrow=c(4,1))
-p = seq(0,1,by=0.01)
+prob.s3.2 = function( n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total){
+  
+  p.i = (y.j+y.g2)/t
+  
+  g = y.g2/(y.g2+y.j)
+
+  ps1=prob.s1.2( n = n, y =y, y.o = y.o, p.v = p.v , t = t )
+  pg1=prob.germination2( n = n, y = y, p.v = p.v)
+  ps2=prob.s2.2(n = n, y = y, y.o = y.o, p.v = p.v)
+  
+  p.vj = ifelse(p.v2 < p.v, p.v^(2/3)*p.v2^(1/3), p.v2^(1/3))
+  
+  p = ((1-g)*(p.vj^(1/3))+g)*p.i*(1/(ps1*(1-pg1)*ps2))
+  return(p)
+}
+
+# if PV2(p,r)<PV1(p,r)
+# PVjan=(PV1(p,r)^.66667)*(PV2(p,r)^.33333);
+# else
+#   PVjan=PV2(p,r)^.33333;
+
+
+
+prob.s3(n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total)
+prob.s3.2(n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total)
+
+
+pdf(file="/Users/Gregor/Dropbox/clarkiaSeedBanks/products/figures/appendix/comparison.pdf",
+    width=9,height=3)
+par(mfrow=c(1,4))
+p = seq(0,1,by=0.001)
 plot(p, prob.s1(n = intact, y = germinant, y.o = october.intact, p.v = p ),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 16,
      xlab="Probability of viability",
-     ylab="Probability of survival",type='l')
+     ylab="Probability of survival",type='l',
+     main = "s1")
 abline(h=(intact+germinant)/(100),lty='dashed')
 
-p = seq(0,1,by=0.05)
+p = seq(0,1,by=0.025)
 points(p, prob.s1.2(n = intact, y = germinant, y.o = october.intact, p.v = p),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 4,col='red')
 
-p = seq(0,1,by=0.01)
+p = seq(0,1,by=0.001)
 plot(p, prob.germination( n = intact, y = germinant, p.v = p ),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 16,
      xlab="Probability of viability",
-     ylab="Probability of germination",type='l')
+     ylab="Probability of germination",type='l',
+     main = "g1")
 abline(h=germinant/(intact+germinant),lty='dashed')
 
-p = seq(0,1,by=0.05)
+p = seq(0,1,by=0.025)
 points(p, prob.germination2( n = intact, y = germinant, p.v = p ),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 4,col='red')
 
 
-p = seq(0,1,by=0.01)
+p = seq(0,1,by=0.001)
 plot(p, prob.s2(n = intact, y = germinant, y.o = october.intact, p.v = p ),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 16,
      xlab="Probability of viability",
-     ylab="Probability of survival",type='l')
+     ylab="Probability of survival",type='l',
+     main = "s2")
 abline(h=(october.intact)/(intact),lty='dashed')
 
-p = seq(0,1,by=0.05)
+p = seq(0,1,by=0.025)
 points(p, prob.s2.2(n = intact, y = germinant, y.o = october.intact, p.v = p),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 4,col='red')
 
-p = seq(0,1,by=0.01)
-plot(p, prob.s3(n = intact, y =germinant, y.o = october.intact, y.j = january.intact, p.v = p, t = total ),
+# year 2 viability
+p.v2 = 1
+
+#par(mfrow=c(1,1))
+p = seq(0,1,by=0.001)
+plot(p, prob.s3(n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total ),
      xlim=c(0,1), ylim=c(0,1),
      cex = .5, pch = 16,
      xlab="Probability of viability",
-     ylab="Probability of survival",type='l')
-abline(h=january.intact/(total),lty='dashed')
+     ylab="Probability of survival",type='l',
+     main='s3')
+abline(h=(january.2.intact+january.2.germ)/(october.intact),lty='dashed')
+abline(h=1,lty='solid')
+# abline(v=2/3)
 
-# p = seq(0,1,by=0.05)
-# points(p, prob.s2.2(n = intact, y = germinant, y.o = october.intact, p.v = p),
-#        xlim=c(0,10), ylim=c(0,1),
-#        cex = .5, pch = 4,col='red')
+p = seq(0,1,by=0.025)
+points(p, prob.s3.2(n = intact, y =germinant, y.o = october.intact, y.j = january.2.intact, y.g2 = january.2.germ, p.v = p , t = total),
+       xlim=c(0,10), ylim=c(0,1),
+       cex = .5, pch = 4,col='red')
+dev.off()
 
 
 
